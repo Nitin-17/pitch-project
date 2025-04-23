62b2845aff4f5380992a812ca6309e6b56963485
nitin-17 <167026966+nitin-0018@users.noreply.github.com>
2025-04-23 23:27:44 +0530

    cache and Live content API added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import "../../app/globals.css";
 import SearchForm from "@/components/SearchForm";
 import StartupCard from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { StartupCardType } from "@/components/StartupCard";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({
   searchParams,
@@ -13,7 +13,7 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
   return (
     <>
@@ -40,6 +40,7 @@ export default async function Home({
           )}
         </ul>
       </section>
+      <SanityLive />
     </>
   );
 }
