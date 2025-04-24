4979ba19d335207d0fca4d0399c743a0e30d37eb
nitin-17 <167026966+nitin-0018@users.noreply.github.com>
2025-04-24 15:34:26 +0530

    PPR added in single post fetching

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -9,3 +9,12 @@ export const STARTUPS_QUERY = defineQuery(
     views, description, category, image
 }`
 );
+
+export const STARTUPS_BY_ID_QUERY = defineQuery(
+  `*[_type=="startup" && _id==$id][0]{
+  _id, title,slug, _createdAt,author->{
+    name, _id, image, bio
+  },
+    views, description, category, image, pitch
+}`
+);
